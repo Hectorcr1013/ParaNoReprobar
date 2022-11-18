07bde64e1bfc12c76085aa4e1f6691efe8f6519b
Hector <Hector@DESKTOP-HKDERPS>
2022-11-18 13:39:59 -0700

    Paquetes y clases agregados

---
+++ src/mx/itson/paranoreprobar/enumeradores/FormaPago.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package mx.itson.paranoreprobar.enumeradores;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ *
+ * @author Hector
+ */
+public enum FormaPago {
+    @SerializedName("1")
+    EFECTIVO,
+    @SerializedName("2")
+    TARJETA,        
+}
